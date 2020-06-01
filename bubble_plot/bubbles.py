4b9452434b2861e8ba51379c2a2be118cff996c2
Jonathan Alvarsson <jonathan.alvarsson@gmail.com>
2020-06-01 17:08:29 +0200

    Added axes

--- bubble_plot/bubbles.py
+++ bubble_plot/bubbles.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-sns.set_style("whitegrid")
-sns.set_color_codes()
+sns.set_style("ticks")
+sns.set_context("notebook")
 
 # create data
 x = np.array(['A', 'A', 'A', 'A', 'N', 'N', 'N', 'N'])
@@ -13,9 +13,15 @@ z = np.array([9, 372, 391, 335, 1, 164, 2339, 1173])
 # use the scatter function
 plt.scatter(x, y, s=z*1)
 plt.margins(.3)
+plt.xlabel("Observed")
+plt.ylabel("Predicted")
+plt.title("Confidence: XXX")
+
 
 for xi, yi, zi in zip(x, y, z):
     plt.annotate(zi, xy=(xi, yi), xytext=(np.sqrt(zi)/2.+5, 0),
                  textcoords="offset points", ha="left", va="center")
+
+sns.despine(offset=10, trim=True)
 
 plt.show()
