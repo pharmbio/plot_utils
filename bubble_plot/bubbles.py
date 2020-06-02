376f1fbfe87592e6d073f738e6d1a3b97184219b
Jonathan Alvarsson <jonathan.alvarsson@gmail.com>
2020-06-02 10:17:52 +0200

    Introduced bubble size scaling

--- bubble_plot/bubbles.py
+++ bubble_plot/bubbles.py
@@ -8,18 +8,21 @@ sns.set_context("notebook")
 # create data
 x = np.array(['A', 'A', 'A', 'A', 'N', 'N', 'N', 'N'])
 y = np.array(['Both', 'A', 'N', 'Null', 'Both', 'A', 'N', 'Null', ])
-z = np.array([9, 372, 391, 335, 1, 164, 2339, 1173])
+#z = np.array([9, 372, 391, 335, 1, 164, 2339, 1173])
+z = np.array([5000, 372, 391, 335, 1, 2500, 2500, 2500])
+
+z_scaled = np.multiply( np.divide(z,z.max()), 2500 )
 
 # use the scatter function
-plt.scatter(x, y, s=z*1)
+plt.scatter(x, y, s=z_scaled)
 plt.margins(.3)
 plt.xlabel("Observed")
 plt.ylabel("Predicted")
 plt.title("Confidence: XXX")
 
-
-for xi, yi, zi in zip(x, y, z):
-    plt.annotate(zi, xy=(xi, yi), xytext=(np.sqrt(zi)/2.+5, 0),
+for xi, yi, zi, z_si in zip(x, y, z, z_scaled):
+    #print(xi,yi,zi,z_si)
+    plt.annotate(zi, xy=(xi, yi), xytext=(np.sqrt(z_si)/2.+5, 0),
                  textcoords="offset points", ha="left", va="center")
 
 sns.despine(offset=10, trim=True)
